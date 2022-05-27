a84cb8f177bcc8a90a33e6057312cd8adcb7c011
Michael Steven Pineda Torres <stevenpnd@gmail.com>
2022-05-26 22:05:23 -0500

    Fix bug in audioplayer

--- src/components/AudioPlayer.js
+++ src/components/AudioPlayer.js
@@ -3,163 +3,173 @@ import { View, ScrollView, Text, Button, StyleSheet } from 'react-native'
 import Sound from "react-native-sound";
 import Slider from "@react-native-community/slider";
 
-const AudioPlayer = ({ audioPath }) => {
+export default class AudioPlayer extends React.Component {
 
-    const [current, setCurrent] = useState(0);
-    const [duration, setDuration] = useState(0);
-    const [state, setState] = useState("paused");
-    const [music, setMusic] = useState(null);
-    const isEditing = useRef(false);
-    const timeout = useRef(null);
+    constructor(props) {
+        super(props);
+        this.state = {
+            playState: 'paused', //playing, paused
+            playSeconds: 0,
+            duration: 0
+        }
+        this.sliderEditing = false;
+    }
 
-    const playComplete = (success) => {
-        if (music) {
+    componentDidMount() {
+        this.play();
+
+        this.timeout = setInterval(() => {
+            if (this.sound && this.sound.isLoaded() && this.state.playState == 'playing' && !this.sliderEditing) {
+                this.sound.getCurrentTime((seconds, isPlaying) => {
+                    this.setState({ playSeconds: seconds });
+                })
+            }
+        }, 100);
+    }
+    componentWillUnmount() {
+        if (this.sound) {
+            this.sound.release();
+            this.sound = null;
+        }
+        if (this.timeout) {
+            clearInterval(this.timeout);
+        }
+    }
+
+    onSliderEditStart = () => {
+        this.sliderEditing = true;
+    }
+    onSliderEditEnd = () => {
+        this.sliderEditing = false;
+    }
+    onSliderEditing = value => {
+        if (this.sound) {
+            this.sound.setCurrentTime(value);
+            this.setState({ playSeconds: value });
+        }
+    }
+
+    play = async () => {
+        if (this.sound) {
+            this.sound.play(this.playComplete);
+            this.setState({ playState: 'playing' });
+        } else {
+            const filepath = this.props.audioPath;
+
+            this.sound = new Sound(filepath, Sound.MAIN_BUNDLE, (error) => {
+                if (error) {
+                    console.log('failed to load the sound', error);
+                    Alert.alert('Notice', 'audio file error. (Error code : 1)');
+                    this.setState({ playState: 'paused' });
+                } else {
+                    this.setState({ playState: 'playing', duration: this.sound.getDuration() });
+                    this.sound.play(this.playComplete);
+                }
+            });
+        }
+    }
+    playComplete = (success) => {
+        if (this.sound) {
             if (success) {
                 console.log('successfully finished playing');
             } else {
                 console.log('playback failed due to audio decoding errors');
+                Alert.alert('Notice', 'audio file error. (Error code : 2)');
             }
-            setState('paused');
-            setCurrent(0);
-            music.setCurrentTime(0);
+            this.setState({ playState: 'paused', playSeconds: 0 });
+            this.sound.setCurrentTime(0);
         }
     }
 
-    const play = async () => {
-
-        if (music) {
-            music.play(playComplete);
-            setState('playing');
-        } else {
-            const temp = new Sound(audioPath, Sound.MAIN_BUNDLE, (error) => {
-                if (error) {
-                    console.log('no se pudo cargar el sonido', err)
-                    return;
-                } else {
-                    setState('playing');
-                    setDuration(temp.getDuration());
-                    temp.play(playComplete);
-                }
-            });
-            setMusic(temp);
-        };
-    }
-
-
-    useEffect(() => {
-
-        timeout.current = setInterval(() => {
-            if (music && music.isLoaded() && state === "playing" && !isEditing.current) {
-
-                music.getCurrentTime((seconds, isPlaying) => {
-                    current(seconds);
-                })
-            }
-        }, 500);
-
-        return () => {
-            if (music) {
-                music.release();
-                setMusic(null);
-            }
-            if (timeout.current) {
-                clearInterval(timeout.current);
-            }
+    pause = () => {
+        if (this.sound) {
+            this.sound.pause();
         }
-    }, []);
 
-
-    const onSliderEditStart = () => {
-        isEditing.current = true;
+        this.setState({ playState: 'paused' });
     }
 
-    const onSliderEditEnd = () => {
-        isEditing.current = false;
-    }
-
-    const onSliderEditing = value => {
-        if (music) {
-            music.setCurrentTime(value);
-            setCurrent(value);
+    jumpPrev15Seconds = () => { this.jumpSeconds(-15); }
+    jumpNext15Seconds = () => { this.jumpSeconds(15); }
+    jumpSeconds = (secsDelta) => {
+        if (this.sound) {
+            this.sound.getCurrentTime((secs, isPlaying) => {
+                let nextSecs = secs + secsDelta;
+                if (nextSecs < 0) nextSecs = 0;
+                else if (nextSecs > this.state.duration) nextSecs = this.state.duration;
+                this.sound.setCurrentTime(nextSecs);
+                this.setState({ playSeconds: nextSecs });
+            })
         }
     }
 
-
-    const pause = () => {
-        if (music)
-            music.pause();
-
-        setState('paused');
-    }
-
-    const stop = () => {
-        if (music) {
-            music.stop();
-            setCurrent(0);
-        }
-        setState('paused');
-    }
-
-    const getAudioTimeString = (seconds) => {
+    getAudioTimeString(seconds) {
         const h = parseInt(seconds / (60 * 60));
         const m = parseInt(seconds % (60 * 60) / 60);
         const s = parseInt(seconds % 60);
 
         return ((h < 10 ? '0' + h : h) + ':' + (m < 10 ? '0' + m : m) + ':' + (s < 10 ? '0' + s : s));
-    };
+    }
 
+    render() {
 
+        const currentTimeString = this.getAudioTimeString(this.state.playSeconds);
+        const durationString = this.getAudioTimeString(this.state.duration);
 
-    return (
-        <ScrollView>
-            <View style={styles.btnContainer}>
-                <Button onPress={play} title="Reproducir">
-                </Button>
-                <Button onPress={pause} title="Pausar">
-                </Button>
-                <Button onPress={stop} title="Reiniciar">
-                </Button>
+        return (
+            <View style={styles.container}>
 
-
+                <View style={styles.btnContainer}>
+                    {this.state.playState == 'playing' &&
+                        <Button onPress={this.pause} title="Pausar" />
+                    }
+                    {this.state.playState == 'paused' &&
+                        <Button onPress={this.play} title="Reproducir" />
+                    }
+                </View>
+                <View style={styles.sliderContainer}>
+                    <Text style={styles.strTime}>{currentTimeString}</Text>
+                    <Slider
+                        onTouchStart={this.onSliderEditStart}
+                        onTouchEnd={this.onSliderEditEnd}
+                        onValueChange={this.onSliderEditing}
+                        value={this.state.playSeconds} maximumValue={this.state.duration} maximumTrackTintColor='gray' minimumTrackTintColor='white' thumbTintColor='white'
+                        style={styles.slider} />
+                    <Text style={styles.strTime}>{durationString}</Text>
+                </View>
             </View>
-            <View style={styles.sliderContainer}>
-                <Text> {getAudioTimeString(current)}</Text>
-                <Slider
-                    onTouchStart={onSliderEditStart}
-                    onTouchEnd={onSliderEditEnd}
-                    onValueChange={onSliderEditing}
-                    value={current}
-                    maximumValue={duration}
-                    maximumTrackTintColor='gray'
-                    minimumTrackTintColor='white'
-                    thumbTintColor='white'
-                    style={styles.slider}
-                />
-
-                <Text> {getAudioTimeString(duration)}</Text>
-            </View>
-        </ScrollView>
-    )
+        )
+    }
 }
-export default AudioPlayer;
+
 
 const styles = StyleSheet.create({
-    btnContainer: {
+    container: {
         flex: 1,
-        flexDirection: "row",
-        justifyContent: "space-evenly"
+        justifyContent: 'center',
+        backgroundColor: 'black'
+    },
+
+    btnContainer: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        marginVertical: 15
     },
 
     sliderContainer: {
-        flex: 1,
-        flexDirection: "row",
-        marginTop: 15,
-        marginBottom: 15
+        marginVertical: 15,
+        marginHorizontal: 15,
+        flexDirection: 'row'
+    },
+
+    strTime: {
+        color: 'white',
+        alignSelf: 'center'
     },
 
     slider: {
-        flex: 8,
-        backgroundColor: "red",
-        marginHorizontal: 8
+        flex: 1,
+        alignSelf: 'center',
+        marginHorizontal: Platform.select({ ios: 5 })
     }
 })
